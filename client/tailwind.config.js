486932a67e49101ebf412218557016b3f6f633a5
Aarush Aggarwal <76877384+Aarushaggarwal-theone@users.noreply.github.com>
2022-10-01 15:19:43 +0530

    y

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/pages/styles.{html,js,css}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
