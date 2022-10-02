80d8f51ee2d181266e5dd19ef75d588527d79eab
Aarush Aggarwal <76877384+Aarushaggarwal-theone@users.noreply.github.com>
2022-10-02 12:54:34 +0530

    navbar

---
+++ client/src/apps/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../components/navbar";
+
+export default function Dashboard() {
+  return (
+    <div className="flex">
+      <Navbar/>
+      
+    </div>
+  );
+}
