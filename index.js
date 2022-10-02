774eeec930a8942a7045fda9978882e0e51d764a
raptor1820 <57589076+raptor1820@users.noreply.github.com>
2022-10-02 13:37:40 +0530

    added lib folder, made skeleton for nodemailer and cleaned up index.js

--- index.js
+++ index.js
@@ -1,81 +1,104 @@
-import express from "express"
-import * as http from "http"
-import dotenv from "dotenv"
-import bodyParser from "body-parser"
-import cors from "cors"
-import mongoose from "mongoose"
+import express from "express";
+import * as http from "http";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import main from "./lib/mailer";
+import * as jwt from "jsonwebtoken";
 
-import * as jwt from jsonwebtoken
+const app = express();
+dotenv.config();
 
-const app = express()
-dotenv.config()
-
-
-mongoose.connect(process.env.HEALTHCARE_DB_URI, {
+mongoose
+  .connect(process.env.HEALTHCARE_DB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to database successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // app.set('view engine', 'ejs')
 
-app.use(express.json())
-app.use(bodyParser.json())
-const server = http.Server(app)
+app.use(express.json());
+app.use(bodyParser.json());
+const server = http.Server(app);
 
-app.use(express.static("public"))
-app.use('/assets', express.static("assets"))
+app.use(express.static("public"));
+app.use("/assets", express.static("assets"));
 
 app.get("/", (req, res) => {
-    res.json("yo")
-})
+  res.json("yo");
+});
 
-app.post('/api/register', async (req, res) => {
-    console.log(req.body)
-    if (! req.body.username || !req.body.email || !req.body.password || !req.body.confirmation) {
-        return res.json({ status: 'error', error: 'Missing fields.'})
+app.post("/api/register", async (req, res) => {
+  console.log(req.body);
+  if (
+    !req.body.username ||
+    !req.body.email ||
+    !req.body.password ||
+    !req.body.confirmation
+  ) {
+    return res.json({ status: "error", error: "Missing fields." });
+  }
+
+  var { username, email, password, confirmation } = req.body;
+  email = email.toLowerCase();
+
+  if (typeof username !== "string") {
+    return res.json({ status: "error", error: "Invalid username." });
+  }
+
+  if (!validator.validate(email)) {
+    return res.json({ status: "error", error: "Invalid E-mail" });
+  }
+
+  if (!(password.length >= 7)) {
+    return res.json({
+      status: "error",
+      error: "Password is too small. Should be atleast 7 characters.",
+    });
+  }
+
+  if (password != confirmation) {
+    return res.json({ status: "error", error: "Passwords don't match." });
+  }
+  var response;
+  try {
+    response = await User.create({
+      username,
+      email,
+      password,
+    });
+    console.log("User created successfully " + response);
+  } catch (error) {
+    if (error.code === 11000) {
+      // duplicate username or e-mail
+      return res.json({
+        status: "error",
+        error: "Username/E-mail already in use.",
+      });
     }
+    throw error;
+  }
 
-    var { username, email, password, confirmation } = req.body
-    email = email.toLowerCase()
+  const token = jwt.sign(
+    { id: response._id, username: response.username },
+    process.env.JWT_SECRET
+  );
 
-    if (typeof(username) !== 'string') {
-        return res.json({ status: 'error', error: 'Invalid username.'})
-    }
+  res.cookie("token", token, { maxAge: 1000 * 60 * 60 * 24 * cookieAge });
+  res.json({ status: 200 });
+});
 
-    if (!validator.validate(email)) {
-        return res.json({ status: 'error', error: 'Invalid E-mail'})
-    }
-
-    if (!(password.length >= 7)) {
-        return res.json({ status: 'error', error: 'Password is too small. Should be atleast 7 characters.'})
-    }
-
-    if (password != confirmation) {
-        return res.json({ status: 'error', error: "Passwords don't match."})
-    }
-    var response;
-    try {
-        response = await User.create({
-            username,
-            email,
-            password
-        })
-        console.log("User created successfully " + response)
-    } catch (error) {
-        if (error.code === 11000) {
-            // duplicate username or e-mail
-            return res.json({ status: 'error', error: 'Username/E-mail already in use.'})
-        }
-        throw error
-    }
-
-    const token = jwt.sign({ id: response._id, username: response.username }, process.env.JWT_SECRET)
-
-    res.cookie('token', token, { maxAge: 1000*60*60*24*cookieAge })
-    res.json({status: 200})
-
-})
-
-const listener = server.listen(process.env.PORT || 3000, () => {
-	console.log(`Listening on ${listener.address().port}`)
-})
+const listener = server.listen(process.env.PORT || 3000, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Listening on ${listener.address().port}`);
+  }
+});
