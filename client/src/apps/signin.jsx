0554e0b1d4f285896c702e6aa5cd0c760829fcb7
Aarush Aggarwal <76877384+Aarushaggarwal-theone@users.noreply.github.com>
2022-10-02 16:56:45 +0530

    sign-in-fixed

--- client/src/apps/signin.jsx
+++ client/src/apps/signin.jsx
@@ -3,9 +3,9 @@ import React from "react";
 import SignUp from "./signup";
 export default function SignIn() {
   return (
-    <div className="flex items-center justify-center p-80 bg-sign-up-light dark:bg-sign-up-dark text-dark-purple dark:text-white">
-      <div className="bg-light-white dark:bg-dark-purple h-fit p-14 max-w-screen shadow-10xl items-center justify-center rounded-2xl">
-        <div className="m-auto items-center justify-center text-center">
+    <div className="flex items-center justify-center p-64 bg-sign-up-light dark:bg-sign-up-dark text-dark-purple dark:text-white">
+      <div className="bg-light-white dark:bg-dark-purple h-fit w-1/2 min-w-fit p-16 max-w-screen shadow-10xl items-center justify-center rounded-2xl">
+        <div className="m-auto items-center justify-center text-center w-full">
         <h1 className="text-5xl font-bold">Log In</h1>
         <p className="text-lg mt-5">
           If you don't have an account{" "}
@@ -14,36 +14,29 @@ export default function SignIn() {
           </span>
         </p>
         </div>
-        <form action="/action_page.php" className="my-5">
-          <div className="flex">
-            <div className="my-5 mr-5">
-          <label for="fname" className="text-2xl">First name</label>
-          <br />
-          <input type="text" id="fname" required  name="fname" placeholder="John" className="text-2xl p-2 mt-5 bg-gray-200 dark:bg-gray-200"/>
-          <br />
-          </div> <div className="my-5 ml-5">
-          <label for="lname" className=" text-2xl">Last name</label>
-          <br />
-          <input type="text" id="lname" required name="lname" placeholder="Doe" className=" p-2 text-2xl mt-5  bg-gray-200 dark:bg-gray-200"/>
-          <br />
-          </div>
-          <br />
-          </div>
+        <form action="/action_page.php" className="items-center justify-center w-full">
           <div  className="mt-5">
           <label for="email" className=" text-2xl mt-14">Email</label>
           <br />
-          <input type="email" required id="email" name="email" placeholder="abcdgtgt@xyz.com" className="mt-5 p-5 w-full  bg-gray-200 dark:bg-gray-200"/>
+          
+          <input type="email" required id="email" name="email" placeholder="abcdgtgt@xyz.com" className="mt-10 p-5 w-full  bg-gray-200 dark:bg-gray-200"/>
           <br />
           </div>
+          <br />
+          <br />
           <div  className="mt-5">
           <label for="password" className=" text-2xl mt-14">Password</label>
           <br />
-          <input type="password" required id="password" name="pwd"placeholder="***********" className="mt-5 p-5 w-full  bg-gray-200 dark:bg-gray-200"/>
+          <input type="password" required id="password" name="pwd"placeholder="***********" className="mt-10 mb-10 p-5 w-full  bg-gray-200 dark:bg-gray-200"/>
           <br />
-          </div>
+          </div>          <br />
+          <br />
+          <br />
+
           <div className="flex items-center justify-center text-center">
-          <input type="submit"  value="Submit" className="p-5 bg-dark-purple rounded-2xl m-5 w-1/3 text-white"/>
+          <input type="submit"  value="Submit" className="p-5 bg-dark-purple hover:bg-gradient-to-r from-gray-800 to-gray-600 dark:bg-gray-600 dark:hover:bg-gradient-to-r rounded-2xl mb-10 w-1/3 text-white"/>
           </div>
+
         </form>
       </div>
     </div>
