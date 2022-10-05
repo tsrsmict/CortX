68831ecdfbd56395eb18e27f058788f74a332b5d
notskamr <66314478+notskamr@users.noreply.github.com>
2022-10-05 11:43:50 +0530

    renamed test.js to test. changed to self-ref model

--- models/test.js
+++ models/test.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose'
 
 const TestSchema = new mongoose.Schema(
 {
+    userID: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User'},
     type: { type: String, required: true },
     data: { type: String, required: true },
 }, { collection: 'tests' , timestamps: true})
