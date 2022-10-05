b0b9ea5b6ebf8fd91e41eccdc0b94d6db5f9e7d9
notskamr <66314478+notskamr@users.noreply.github.com>
2022-10-05 11:40:11 +0530

    fixed unnecessary username field

--- models/tests.js
+++ models/tests.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose'
 
 const TestSchema = new mongoose.Schema(
 {
-    username: { type: String, required: true },
     type: { type: String, required: true },
     data: { type: String, required: true },
 }, { collection: 'tests' , timestamps: true})
