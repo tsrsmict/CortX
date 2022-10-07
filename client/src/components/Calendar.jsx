835b92243fb464224452b8d8b782bae6dde65a5c
Aarush Aggarwal <76877384+Aarushaggarwal-theone@users.noreply.github.com>
2022-10-07 18:34:10 +0530

    test-commit merge Co-authored-by: Shreedhar Raj <Shreedhar-Raj@users.noreply.github.com>

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import FullCalendar from '@fullcalendar/react' // must go before plugins
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugingtgtgtg!
 import ReminderMake from './reminder_make'
 export default class DemoApp extends React.Component {
   render() {
