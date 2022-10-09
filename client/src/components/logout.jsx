54709b641786b6352110796134ec9d0b3335777d
notskamr <66314478+notskamr@users.noreply.github.com>
2022-10-09 12:53:38 +0530

    fixed auth, fixed logout.

--- client/src/components/logout.jsx
+++ client/src/components/logout.jsx
@@ -26,17 +26,19 @@ class LogoutModule extends Component {
     return (
       <div className="grid min-h-screen bg-sign-up-light dark:bg-sign-up-dark place-items-center">
         <div className="transition ease-in duration-200 hover:backdrop-blur-2xl border-1 border-gray-700 rounded-2xl w-11/12 p-12 bg-sign-up bg-white/[0.7] dark:bg-zinc-900/[0.8] shadow-10xl dark:shadow-zinc-800 sm:w-1/2 lg:w-5/12">
-          <h1 className="text-5xl font-semibold dark:text-white">Log Out</h1>
+          <h1 className="text-5xl font-semibold dark:text-white text-center">
+            Log Out
+          </h1>
           <br />
           <button
             onClick={(e) => this.logOut(e)}
-            className=" w-full py-3 mt-6 font-medium tracking-widest text-white dark:text-black uppercase bg-black shadow-lg dark:bg-white dark:hover:text-white focus:outline-none hover:bg-gray-600 duration-300 hover:shadow-none"
+            className=" w-full py-3 mt-6 font-medium tracking-widest text-white  uppercase  shadow-lg focus:outline-none duration-300 hover:shadow-none bg-red-500 hover:bg-red-400 hover:text-gray-800"
           >
             Confirm!
           </button>
           <button
             onClick={this.dashboardRedir}
-            className=" w-full py-3 mt-6 font-medium tracking-widest text-white dark:text-black uppercase bg-black shadow-lg dark:bg-white dark:hover:text-white focus:outline-none hover:bg-gray-600 duration-300 hover:shadow-none"
+            className=" w-full py-3 mt-6 font-medium tracking-widest text-black uppercase shadow-lg  hover:invert focus:outline-none  duration-300 hover:shadow-none bg-green-400"
           >
             Return to Dashboard
           </button>
