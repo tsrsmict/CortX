06d018543d7d2ef0ef1bd06fe1fca5c14251ced2
notskamr <66314478+notskamr@users.noreply.github.com>
2022-10-09 22:40:03 +0530

    fileRouter godamn works.

--- middlewares/checkUser.js
+++ middlewares/checkUser.js
@@ -5,18 +5,18 @@ dotenv.config();
 const checkUser = async (req, res, next) => {
   if (req.cookies.jwtToken == null)
     return res
-      .status(400)
+      .status(403)
       .json({ status: "error", error: "No jwtToken in cookies." });
   let cookie;
   try {
     cookie = jsonwebtoken.verify(req.cookies.jwtToken, process.env.JWT_SECRET);
     if (!(await User.exists({ _id: cookie.id })))
       return res
-        .status(400)
+        .status(403)
         .json({ status: "error", error: "User not found." });
   } catch (err) {
     return res
-      .status(400)
+      .status(403)
       .json({ status: "error", error: "Invalid jwtToken in cookies." });
   }
 
