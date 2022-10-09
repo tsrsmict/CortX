1c01c702ae9d8aad1ac2c4fcbbe9d6c1d8e34628
notskamr <sahnivarun62@gmail.com>
2022-10-09 16:19:05 +0530

    fixed

--- client/src/index.js
+++ client/src/index.js
@@ -9,9 +9,15 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Link,
+  useLocation,
+  Outlet,
 } from "react-router-dom";
-import SigninModule from "./components/signin_m"
+import { Navigate } from "react-router-dom";
+
+import SigninModule from "./components/signin_m";
 import SignupModule from "./components/signup_m";
+import LogoutModule from "./components/logout";
 import Table from "./components/table";
 import Dashboard from "./apps/Dashboard";
 // import Files from "./components/Files";
@@ -20,113 +26,119 @@ import Contacts from "./components/Contacts";
 // import Recordings from "./components/Recordings";
 // import Files_M from './components/Files';
 // import Navbar from './components/navbar';
-import Files from './apps/File_M/Files'
+import Files from "./apps/File_M/Files";
 // import Mr from './apps/File_M/mr';
 // import NavBar from "./components/navbar";
 // import Malendar from "./apps/calendar";
-import Upload from './components/upload'
+import Upload from "./components/upload";
 import ReminderMake from "./components/reminder_make";
-const TableData = [{
-  title: "White Blood Cell (WBC) (Leukocytes) Count",
-  value: 6250,
-  unit: 'cells/cu.mm',
-  bri_start: 4000,
-  bri_end: 10500,
-  color: false
-}, {
-  title: 'Red blood Cell (Wbc) Erthrocytes Count',
-  value: 4.94,
-  unit: 'milli/cu.mm',
-  bri_start: 4.7,
-  bri_end: 6.0,
-  color: true
-}, {
-  title: 'Platelet Count',
-  value: 173,
-  unit: '10^3/microliter',
-  bri_start: 150,
-  bri_end: 450,
-  color: false
-},
-{
-  title: 'Absolute Monocyte Count',
-  value: 500,
-  unit: '/c.mm',
-  bri_start: 200,
-  bri_end: 1000,
-  color: true
-},
-{
-  title: 'Absolute Eosinophilis Count',
-  value: 375,
-  unit: '/c.mm',
-  bri_start: 200,
-  bri_end: 1000,
-  color: false
-},
-{
-  title: 'Absolute Lymphocyte Count',
-  value: 500,
-  unit: '/c.mm',
-  bri_start: 1000,
-  bri_end: 3000,
-  color: true
-}, {
-  title: 'Absolute Neutrophilis Count',
-  value: 3313,
-  unit: '/c.mm',
-  bri_start: 2000,
-  bri_end: 7000,
-  color: false
-},
-{
-  title: 'Absolute Neutrophilis Count',
-  value: 3313,
-  unit: '/c.mm',
-  bri_start: 2000,
-  bri_end: 7000,
-  color: true
+import axios from "axios";
+
+const RequireAuth = () => {
+  const location = useLocation();
+  const [auth, setAuth] = React.useState();
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useLayoutEffect(() => {
+    const authCheck = async () => {
+      setIsLoading(true);
+      try {
+        /* =============== HERE CALL MY API CHECK =============== */
+
+        // await the asynchronous logic
+        const auth = await axios.get("api/users/checkAuth");
+
+        setAuth(auth.data.auth);
+      } catch (error) {
+        // handle any Promise rejections, errors, etc...
+        setAuth(false); // or unauthorized value
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    authCheck();
+
+    return () => {
+      // cancel/clean up any pending/in-flight asynchronous auth checks
+    };
+  }, [location.pathname]); // <-- check authentication when route changes
+
+  if (isLoading) {
+    return null; // or loading spinner, etc...
+  }
+
+  return auth ? ( // or auth property if object, i.e. auth.isAuthenticated, etc...
+    <Outlet />
+  ) : (
+    <Navigate to="/signin" replace />
+  );
+};
+
+function NotFound() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <div style={{ textAlign: "center" }}>
+        <h1 style={{ fontSize: "5em", color: "red" }}>
+          Oops! You seem to be lost.
+        </h1>
+        <p style={{ fontSize: "3em", textDecorationLine: "underline" }}>
+          Here are some helpful links:
+        </p>
+
+        <div
+          style={{
+            display: "flex",
+            gap: "1em",
+            justifyContent: "center",
+            fontSize: "2em",
+          }}
+        >
+          <Link to="/signin">Sign In</Link>
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/dashboard">Dashboard</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-
-]
-
-
-async function checkAuth() {
-  await fetch("/api/users/checkAuth", {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  })
-    .then((res) => res.json())
-    .then((json) => {
-      if (json.auth == false) window.location.replace("/signin");
-    });
-}
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <div>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/signup" element={<SignupModule />}></Route>
+            <Route path="/signin" element={<SigninModule />}></Route>
+            <Route path="/logout" element={<LogoutModule />}></Route>
+            <Route path="/tables" element={<Table />}></Route>
 
-    {/* <Table/> */}
-<Router>
-      <div>
-        <Routes>
-          <Route path="/signup" element={<SignupModule/>}></Route>
-          <Route path="/signin" element={<SigninModule/>}></Route>
-          <Route path="/tables" element={<Table/>}></Route>
-          <Route path="/" render={() => checkAuth() } element={ <Dashboard/> }></Route>
-          <Route path="/files" element={<Files />}></Route>
-          <Route path="/upload" element={<Upload />}></Route>
-          <Route path="/contacts" element={<Contacts />}></Route>
-          <Route path="/calendar" element={<MyCalendar />}></Route>
-          <Route path='/remindermake' element={<ReminderMake/>}></Route>
-        </Routes>
-      </div>
-    </Router>
+            <Route element={<RequireAuth />}>
+              <Route path="/dashboard" element={<Dashboard />}></Route>
+              <Route path="/files" element={<Files />}></Route>
+              <Route path="/upload" element={<Upload />}></Route>
+              <Route path="/contacts" element={<Contacts />}></Route>
+              <Route path="/calendar" element={<MyCalendar />}></Route>
+              <Route path="/remindermake" element={<ReminderMake />}></Route>
+            </Route>
+
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
   </React.StrictMode>
 );
-reportWebVitals()
+reportWebVitals();
 
-
-// 
+//
--- client/src/setupProxy.js
+++
@@ -1,5 +0,0 @@
-import createProxyMiddleware from "http-proxy-middleware";
-
-export default function (app) {
-  app.use(createProxyMiddleware("/api", { target: "http://localhost:5000" }));
-}
