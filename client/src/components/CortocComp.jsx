46b206b745525335b42eadb8688ace6eaa05b9eb
notskamr <66314478+notskamr@users.noreply.github.com>
2022-10-11 23:52:41 +0530

    cortocompanion

--- client/src/components/CortocComp.jsx
+++ client/src/components/CortocComp.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-export default function CortoComp () {
-    const [showModal, setShowModal] = useState(false)
-    return(
-        <div>
-
-            <button
+export default function CortoComp() {
+  const [showModal, setShowModal] = useState(false);
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  return (
+    <div>
+      <button
         className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
         type="button"
         onClick={() => setShowModal(true)}
@@ -14,17 +14,13 @@ export default function CortoComp () {
       </button>
       {showModal ? (
         <>
-          <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-          >
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">
-                    Corto Companion
-                  </h3>
+                  <h3 className="text-3xl font-semibold">Corto Companion</h3>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={() => setShowModal(false)}
@@ -36,13 +32,35 @@ export default function CortoComp () {
                 </div>
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
-                  </p>
+                  <div className="my-4 text-slate-500 text-lg leading-relaxed">
+                    {!formSubmitted ? (
+                      <form>
+                        <input
+                          type="text"
+                          placeholder="Name of Medicine"
+                        ></input>
+                        <input type="text" placeholder="Doses/week"></input>
+                        <input type="text" placeholder="Reason"></input>
+                        <button
+                          type="submit"
+                          onClick={(e) => {
+                            e.preventDefault();
+                            setFormSubmitted(true);
+                          }}
+                        >
+                          Submit
+                        </button>
+                      </form>
+                    ) : (
+                      <h3>
+                        <b>
+                          This medicine may cause complications; nausea,
+                          headaches, fatigue. Pleace consult your doctor about
+                          such before consumption.
+                        </b>
+                      </h3>
+                    )}
+                  </div>
                 </div>
                 {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
@@ -53,20 +71,22 @@ export default function CortoComp () {
                   >
                     Close
                   </button>
-                  <button
+                  {/* <button
                     className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
                     Save Changes
-                  </button>
+                  </button> */}
                 </div>
               </div>
             </div>
           </div>
           <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>) : <></>}
-
+        </>
+      ) : (
+        <></>
+      )}
     </div>
-    )
+  );
 }
